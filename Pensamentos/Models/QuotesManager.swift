f7dcb0da35bcdb3467a899d6fc552912ac9eb376
kleyson tavares <kleyson.tavares@icloud.com>
2021-04-19 23:22:27 -0300

    remoção do desembrulho dos opcional por if let da classe ManagerQuote, para acesso aos dados por uma forma mais segura

--- Pensamentos/Models/QuotesManager.swift
+++ Pensamentos/Models/QuotesManager.swift
@@ -8,15 +8,21 @@
 
 import Foundation
 final class QuotesManager {
-
-    let quotes: [Quote]
     
+    var quotes: [Quote] = []
     init( ) {
-        let fileURL = Bundle.main.url(forResource: "quotes", withExtension: "json")!
-        let jsonData = try! Data(contentsOf: fileURL)
-        let jsonDecoder = JSONDecoder()
-        quotes = try! jsonDecoder.decode([Quote].self, from: jsonData)
-    }
+        if let fileURL = Bundle.main.url(forResource: "quotes", withExtension: "json") {
+            if let jsonData =  try?    Data(contentsOf: fileURL) {
+                let jsonDecoder =  JSONDecoder()
+                do {
+                    if let quotesData = try? jsonDecoder.decode([Quote].self, from: jsonData) {
+                        quotes = quotesData
+                    }}
+                catch  {
+                    print("deu erro")
+                }
+            }}}
+    
     func getRandomQuote () -> Quote {
         let index = Int(arc4random_uniform(UInt32(quotes.count)))
         return quotes[index]
